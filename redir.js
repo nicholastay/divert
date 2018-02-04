8bd299be747e73a825e859a023abb96e1b390bc0
Nicholas Tay <nexerq@gmail.com>
2018-02-04 20:45:46 +1100

    stuff

--- redir.js
+++ redir.js
@@ -1,5 +1,7 @@
 ;(function() {
 
+window.nexdivert_show_links = true;
+
 var webpath = window.location.pathname.split('/').pop();
 if (window.nexdivert.hasOwnProperty(webpath))
 	return (window.location.href = window.nexdivert[webpath]);
